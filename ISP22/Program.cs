d6e771074004d8994f2d4c8ef09e1b3451e5441b
su <rakdav@inbox.ru>
2022-09-14 14:20:25 +0200

    Добавьте файлы проекта.

---
+++ ISP22/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Drawing;
+
+try
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write("Введите а:");
+    double a = double.Parse(Console.ReadLine());
+    Console.Write("Введите b:");
+    double b = double.Parse(Console.ReadLine());
+    Console.Write("Введите c:");
+    double c = double.Parse(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.Green;
+    double res = ((b + Math.Sqrt(b * b + 4 * a * c)) / 2 * a) -
+        Math.Pow(a, 3) * c + Math.Pow(b, -2);
+    Console.WriteLine($"{res:F2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
---
+++ TaskManager/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskManager
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Process[] process;
+        private Timer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            process =Process.GetProcesses();
+            foreach(Process proc in process)
+            {
+                lbProcesses.Items.Add(proc.ProcessName);
+            }
+            timer= new Timer(3000);
+            
+        }
+
+    }
+}
